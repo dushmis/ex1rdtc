fdd4653c72137a9f63491642557ba7c2b84e1e96
dushyant <dushmis@gmail.com>
2014-09-12 21:55:02 +0530

    bug

--- background.js
+++ background.js
@@ -43,21 +43,21 @@ var channel=function(data){
   this.refreshrate=( data.refreshrate ? data.refreshrate < 2000 ? 2000 : data.refreshrate : 2000 )
 }
 
-// appmod.constant("dushyant",{name:"test"});
 
 appmod.factory("EventFactory",function(){
   return {
     onChange:function(value){
+        // console.info("change %s",value);
         chrome.browserAction.getBadgeText({},function(result){
           //chrome.browserAction.setBadgeText({text:(+result+1)+""});
           chrome.browserAction.setBadgeText({text:"New"});
         });
     },
     onFetch:function(data){
-       console.log("fetch ");
+      //  console.log("fetch ");
     },
     onError:function(something){
-      console.error(something);
+      // console.error(something);
     }
   };
 });
@@ -92,21 +92,21 @@ appmod.factory("ChannelFactory",function(ItemFactory){
 });
 
 
-appmod.controller("BackgroundFetchController", function($scope, $http, $templateCache, $interval, ItemFactory, EventFactory, ChannelFactory) {  
-  $scope.fetch = function(scdata) {
+appmod.controller("BackgroundFetchController", function($scope, $http, $templateCache, $interval, ItemFactory, EventFactory, ChannelFactory) {
+  $scope.fetch = function(scdata,dowhat) {
 
     $scope.method = "GET";
     $scope.source = scdata.source;
     $scope.predicate = scdata.predicate;
     $scope.ascorder = scdata.ascorder;
-    
-    //console.log(scdata);
 
-    EventFactory.onFetch($scope.source);
+    // console.log("object %s",scdata);
+
+    EventFactory.onFetch(scdata.source);
 
     var rows_news = [];
     var t=(new Date()).getTime();
-    $scope.url = b64_.e("aHR0cDovL3d3dy5yZWRkaXQuY29tL3Iv") + $scope.source + b64_.e("Ly5qc29uP2xpbWl0") + "=20&tmp="+t;
+    $scope.url = b64_.e("aHR0cDovL3d3dy5yZWRkaXQuY29tL3Iv") + scdata.source + b64_.e("Ly5qc29uP2xpbWl0") + "=20&tmp="+t;
     $scope.code = null;
     $scope.response = null;
 
@@ -116,13 +116,14 @@ appmod.controller("BackgroundFetchController", function($scope, $http, $template
         rows_news.push(a.data);
       });
       var stringed=JSON.stringify(rows_news);
-      var stringed_data=ItemFactory.getSha($scope.source);
+      var stringed_data=ItemFactory.getSha(scdata.source);
 
       var tosave= !(angular.equals(stringed_data,sha1(stringed)));
+      dowhat(scdata.source);
 
       if(tosave){
-        EventFactory.onChange({});
-      	ItemFactory.save($scope.source,stringed);
+        EventFactory.onChange(scdata.source);
+      	ItemFactory.save(scdata.source,stringed);
       }
     }).error(function(data, status) {
       $scope.status = status;
@@ -132,17 +133,19 @@ appmod.controller("BackgroundFetchController", function($scope, $http, $template
 
   var channels = ChannelFactory.get();
   $scope.channels=channels;
-  console.log(channels);
+  // console.log(channels);
   for (var i = 0; i <= channels.length - 1; i++) {
     (function(singleChannel){
-      console.log("sing " + i +  singleChannel);
+      // console.log("sing " + i +  singleChannel + " " + (singleChannel.refreshrate+(i+1)*10));
       stop = $interval(function(){
         $scope.fetch({
           "source":singleChannel.source,
           "predicate":singleChannel.predicate,
           "ascorder":singleChannel.ascorder
+        },function(dowhat){
+          // console.info("do what %s",dowhat);
         });
-      }, singleChannel.refreshrate+i);    
+      }, singleChannel.refreshrate+(i+1)*10);
     })(channels[i]);
   };
 
